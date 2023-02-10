4ec46a728769bab21889452eeebf59c2d219cdca
deepansh <99.deepanshagrawal@gmail.com>
2023-02-10 20:35:15 +0530

    * Adding initial list.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+
+const relatives = ["Uncle1", "Uncle2", "Uncle3", "Uncle4", "Uncle5"];
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        {relatives.map((x, i) => {
+          return <li key={`relativeListItem${i + 1}`}>{x}</li>;
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
